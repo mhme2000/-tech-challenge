3ee6153d4a61224fba1415c532f8ee91ad593e6c
zeyns <marcoslucsrios@gmail.com>
2023-07-04 22:35:47 -0300

    feat: added stock entity

--- src/core/Order/domain/entities/orderItem.entity.ts
+++ src/core/Order/domain/entities/orderItem.entity.ts
@@ -1,10 +1,4 @@
-import {
-  Column,
-  Entity,
-  JoinColumn,
-  OneToOne,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class OrderItem {
